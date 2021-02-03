457e9d296c090f9fbf7abdd1b899afcdb5a453e0
Kashif H Khan <25973476+KashifHK123@users.noreply.github.com>
2021-02-03 18:09:00 +0500

    Create centos7.sh

---
+++ centos7.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+echo "   ___         _    ___  ___ ____   "
+echo "  / __|___ _ _| |_ / _ \/ __|__  |  "
+echo " | (__/ -_) ' \  _| (_) \__ \ / /   "
+echo "  \___\___|_||_\__|\___/|___//_/    "
+echo " OpenVPN Access Server With Unlimited License"
+
+yum update -y
+yum install wget -y
+yum install net-tools -y
+wget https://github.com/KashifHK123/OpenVPN-AS-Unlimited/raw/main/openvpn-as-2.5-CentOS7.x86_64.rpm
+rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
+cd /usr/local/openvpn_as/lib/python2.7/site-packages/
+rm pyovpn-2.0-py2.7.egg
+wget https://github.com/KashifHK123/OpenVPN-AS-Unlimited/raw/main/pyovpn-2.0-py2.7.egg
+cd /usr/local/openvpn_as/bin
+./ovpn-init
+
+
+echo "=========================DONE=============================="
+echo "SETUP ADMIN PASSWORD"
+
+passwd openvpn
+
+
+echo "=========================DONE=============================="
