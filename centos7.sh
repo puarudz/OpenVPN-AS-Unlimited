3e1a1d0d410ebde63098a3f2a9ce7c9da97bce5f
Kashif H Khan <25973476+KashifHK123@users.noreply.github.com>
2021-02-03 18:43:36 +0500

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -8,13 +8,10 @@ echo "  \___\___|_||_\__|\___/|___//_/    "
 echo " OpenVPN Access Server With Unlimited License"
 
 yum update -y
-yum install wget -y
 yum install net-tools -y
-wget https://github.com/KashifHK123/OpenVPN-AS-Unlimited/raw/main/openvpn-as-2.5-CentOS7.x86_64.rpm
 rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
-cd /usr/local/openvpn_as/lib/python2.7/site-packages/
-rm pyovpn-2.0-py2.7.egg
-wget https://github.com/KashifHK123/OpenVPN-AS-Unlimited/raw/main/pyovpn-2.0-py2.7.egg
+rm /usr/local/openvpn_as/lib/python2.7/site-packages/pyovpn-2.0-py2.7.egg
+cp pyovpn-2.0-py2.7.egg /usr/local/openvpn_as/lib/python2.7/site-packages/
 cd /usr/local/openvpn_as/bin
 ./ovpn-init
 
